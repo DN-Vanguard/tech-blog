8ae27a46db4635917f9c35a192f0234f84a1a5d1
DN-Vanguard <Domiltannguyen@outlook.com>
2021-09-25 13:43:41 -0700

    signup, login, logout interface

---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+const loginFormSubmit = async (event) => {
+	event.preventDefault();
+
+	const username = document.querySelector('#username-signup').value.trim();
+	const password = document.querySelector('#password-signup').value.trim();
+
+	if (username && password) {
+		const response = await fetch('/api/users/login', {
+			method: 'POST',
+			body: JSON.stringify( {
+				username,
+				password
+			}),
+			headers: {'Content-Type': 'application/json'},
+		});
+
+		if (response.ok) {
+			document.location.replace('/dashboard');
+		} else {
+			alert('Incorrect login');
+		}
+	}
+};
+
+document
+	.querySelector('.login-form')
+	.addEventListener('submit', loginFormSubmit);
---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+const signupFormSubmit = async (event) => {
+	event.preventDefault();
+
+	const username = document.querySelector('#username-signup').value.trim();
+	const password = document.querySelector('#password-signup').value.trim();
+
+	if (username && password) {
+		console.log('@@@@@@@@@@@');
+		const response = await fetch('/api/users', {
+			method: 'POST',
+			body: JSON.stringify({
+				username,
+				password
+			}),
+			headers: {'Content-Type': 'application/json'},
+		});
+
+		if (response.ok) {
+			document.location.replace('/dashboard');
+		} else {
+			alert('Please fill in \'Username\' & \'Password\'.\n\'Password\' must be a minimum of 8 characters.');
+		}
+	}
+};
+
+document
+	.querySelector('.signup-form')
+	.addEventListener('submit', signupFormSubmit);
