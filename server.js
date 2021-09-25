d928a9d85168f06c7e10ffa5f66188d3720a89ff
DN-Vanguard <Domiltannguyen@outlook.com>
2021-09-25 10:38:03 -0700

    Server.js updated with console log message.

--- server.js
+++ server.js
@@ -33,8 +33,11 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Setup static path to public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening at http://localhost:${PORT}'));
 });
