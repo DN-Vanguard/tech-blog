59ceca564e67ccbca74b64d9b90df4f8e4a6b910
DN-Vanguard <Domiltannguyen@outlook.com>
2021-09-25 10:35:46 -0700

    completed package.json file and connected express and hadnlebars into server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
+// Modules
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const expHandlebars = require('express-handlebars');
+// Files
 const routes = require('./controllers');
-
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
+// Sessions
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+// Express and server ports
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+// Direction to handlebars to helper files
+const handlebars = expHandlebars.create({helpers});
+// parameters
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.secret,
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -19,6 +26,9 @@ const sess = {
 };
 
 app.use(session(sess));
+// Connect express and handlebars
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
