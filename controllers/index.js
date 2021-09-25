c14bc622decbeacea90be3b3ab6604cd08a6279c
DN-Vanguard <Domiltannguyen@outlook.com>
2021-09-25 11:14:00 -0700

    created routes interface.

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+const projectRoutes = require('./projectRoutes');
 
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/projects', projectRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
