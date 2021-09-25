8dab12bf23c17964349fad2464326f357d0ea296
DN-Vanguard <Domiltannguyen@outlook.com>
2021-09-25 11:42:16 -0700

    index.js has associations added.

--- models/index.js
+++ models/index.js
@@ -1,13 +1,29 @@
 const User = require('./User');
 const Project = require('./Project');
+const Comment = require('./Comment');
 
 User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+	foreignKey: 'userId',
+	onDelete: 'CASCADE'
 });
-
 Project.belongsTo(User, {
-  foreignKey: 'user_id'
+	foreignKey: 'userId'
 });
 
-module.exports = { User, Project };
+User.hasMany(Comment, {
+	foreignKey: 'userId',
+	onDelete: 'CASCADE'
+});
+Comment.belongsTo(User, {
+	foreignKey: 'userId'
+});
+
+Project.hasMany(Comment, {
+	foreignKey: 'projectId',
+	onDelete: 'CASCADE'
+});
+Comment.belongsTo(Project, {
+	foreignKey: 'projectId'
+});
+
+module.exports = {User, Project, Comment};
