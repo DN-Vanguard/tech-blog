dbc21da13ee7d31618781c18f290ce29217d0282
DN-Vanguard <Domiltannguyen@outlook.com>
2021-10-21 18:57:42 -0700

    routes are working properly.

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,66 +1,62 @@
-// URL localhost:3001/dashboard
-// Required modules
-const router = require( 'express' ).Router();
+const router = require('express').Router();
 // Required files
-const { Blogpost, User } = require( '../models' );
-const withAuth = require( '../utils/auth' );
-const serialize = require( '../utils/serialize' );
+const { Blogpost, User } = require('../models');
+const withAuth = require('../utils/auth');
+const serialize = require('../utils/serialize');
 
-// Home route
-router.get( '/', withAuth, async ( req, res ) => {
+// DEFAULT
+router.get('/', withAuth, async (req, res) => {
 	try {
-		const blogpostData = await Blogpost.findAll( {
+		const blogpostData = await Blogpost.findAll({
 			include: [
 				{ model: User }
 			],
 			where: {
 				userId: req.session.user_id
 			}
-		} );
+		});
 
-		const blogposts = blogpostData.map( serialize );
+		const blogposts = blogpostData.map(serialize);
 
-		res.render( 'dashboard', {
+		res.render('dashboard', {
 			blogposts,
 			logged_in: req.session.logged_in
-		} );
+		});
 
-	} catch ( err ) {
-		res.status( 500 ).json( err );
+	} catch (err) {
+		res.status(500).json(err);
 	}
-} );
+});
 
-// Add new blogpost view
-router.get( '/newpost', withAuth, async ( req, res ) => {
+// ADD
+router.get('/newpost', withAuth, async (req, res) => {
 	try {
 
-		res.render( 'newBlogpost', {
+		res.render('newBlogpost', {
 			logged_in: req.session.logged_in
-		} );
+		});
 
-	} catch ( err ) {
-		res.status( 500 ).json( err );
+	} catch (err) {
+		res.status(500).json(err);
 	}
-} );
+});
 
-// Edit blogpost view
-router.get( '/:id', withAuth, async ( req, res ) => {
+// EDIT
+router.get('/:id', withAuth, async (req, res) => {
 	try {
 
-		const blogpostData = await Blogpost.findByPk( req.params.id );
+		const blogpostData = await Blogpost.findByPk(req.params.id);
 
-		const blogpost = blogpostData.get( { plain: true } );
+		const blogpost = blogpostData.get({ plain: true });
 
-		res.render( 'editBlogpost', {
+		res.render('editBlogpost', {
 			blogpost,
 			logged_in: req.session.logged_in
-		} );
+		});
 
-	} catch ( err ) {
-		res.status( 500 ).json( err );
+	} catch (err) {
+		res.status(500).json(err);
 	}
-} );
+});
 
-
-// Export
 module.exports = router;
